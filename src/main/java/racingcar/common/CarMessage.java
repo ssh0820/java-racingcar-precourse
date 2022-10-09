5af5df166934a047671b8e2e645e8349f6b363ee
ssh0820 <53414519+ssh0820@users.noreply.github.com>
2022-10-09 19:28:12 +0900

    feat : 공통 문구 추가

---
+++ src/main/java/racingcar/common/CarMessage.java
@@ -0,0 +1,15 @@
+package racingcar.common;
+
+public class CarMessage {
+
+
+    public static final int MIN_NUMBER = 0;
+    public static final int MAX_NUMBER = 9;
+    public static final int MOVE_STANDARD = 4;
+
+    public static final int START_POSITION = 0;
+    public static final int START_ROUND_NO = 1;
+
+    public static final String SEPARATOR = ",";
+    public static final String HYPHEN = "-";
+}
--- src/main/java/racingcar/common/ExcMessage.java
+++ src/main/java/racingcar/common/ExcMessage.java
@@ -1,15 +1,13 @@
 package racingcar.common;
 
-public class Message {
+public class ExcMessage {
     public static final String INPUT = "경주 할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
 
-    public static final String TRY = "시도할 회수는 몇회인가요?";
+    public static final String TRY = "시도할 횟수는 몇회인가요?";
 
     public static final String WINNER = "최종 우승자 : ";
 
-    public static final String HYPHEN = "-";
-
-    public static final String NUMBER_REGEX = "^[1-9]{1}$";
+    public static final String NUMBER_REGEX = "^[0-9]{1}$";
 
     public static final String ERROR_NUMBER = "[ERROR] 시도 횟수는 숫자여야 한다.";
 
