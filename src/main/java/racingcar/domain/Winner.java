131cd9ac0b2dc1f932680b383f754580dbcdfc69
ssh0820 <53414519+ssh0820@users.noreply.github.com>
2022-10-10 01:59:22 +0900

    feat : Winner 객체 구현 및 TEST 검증

--- src/main/java/racingcar/domain/Winner.java
+++ src/main/java/racingcar/domain/Winner.java
@@ -3,24 +3,22 @@ package racingcar.domain;
 import racingcar.common.CarMessage;
 import racingcar.common.ExcMessage;
 
+import java.util.List;
+
 public class Winner {
 
-    private final String carName;
-
-    public Winner(String carName){
-        this.carName = carName;
-    }
+    private  String carName;
 
     public String getCarName(String name){
        return ExcMessage.WINNER+name;
     }
 
-    public String getCarName(String[] names){
+    public String getCarName(List<Car> cars,int maxScore){
         String carName = "";
-
-        for (String name: names
-             ) {
-            carName += name+ CarMessage.SEPARATOR+" ";
+        for (Car car: cars) {
+            if(maxScore == car.getPosition().getPosition()){
+                carName += car.getCarName().getName()+ CarMessage.SEPARATOR+" ";
+            }
         }
         return ExcMessage.WINNER+ carName.substring(0,carName.length()-2);
     }
